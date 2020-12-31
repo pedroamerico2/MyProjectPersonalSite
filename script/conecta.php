b52fad9d87bcd03ae4616afbf7eac7bc95411820
Pedro Américo <pedroamerico2006@gmail.com>
2020-12-31 10:32:13 -0300

    site 0.1
    -Modificação: conecta.php agora tem como host localhost

--- script/conecta.php
+++ script/conecta.php
@@ -1,9 +1,8 @@
 <?php
     abstract class ClassConexao{
-        public function Conectar()
-        {
+        public function Conectar(){
             try{
-                $Con=new PDO("mysql:host=31.220.104.197;dbname=u104395534_contadorSite","u104395534_pedroamerico4","Pol2006@");
+                $Con=new PDO("mysql:host=localhost;dbname=u104395534_contadorSite","u104395534_pedroamerico4","Pol2006@");
                 return $Con;
             }catch (PDOException $Erro){
                 return $Erro->getMessage();
