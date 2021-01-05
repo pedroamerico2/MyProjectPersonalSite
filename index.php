12312761b1e4ca21f45503fe11289a5c0172fffe
Pedro Américo <pedroamerico2006@gmail.com>
2021-01-05 13:29:22 -0300

    edicao 1.0
    -Adicao: style.css incremento de varias personalizações da pagina

--- index.php
+++ index.php
@@ -7,34 +7,47 @@
     <title>Welcome to the jungle</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <nav>
+            <ul>
+                <li>Conteudo 1</li>
+                <li>Option 2</li>
+                <li>Option 3</li>
+                <li>Option 4</li>
+                <li><a href="http://sistemas.pedroamerico.com">sistemas</a></li>
+            </ul>
+        </nav>
+        <h1>Seja Bem Vindo</h1>
+        <p>Site de um entusiasta tecnologico entrando no ramo de desenvolvimento.</p>
+    </header>
     <main>
-        <div class="planoDeFundo">
-            <img src="./img/iconeAdvertencia.png" class="advertencia" alt="">
-            <h1>PAGINA EM CONSTRUÇÃO</h1>
-            <p>Américo agradece a visita :)</p>  
-            <div class = "n-acessos">
-                <h1> Visitante nº:  </h1>
-                <?php
-                    include ("contadorIp.php");
-                    $Visitas = new ClassVisitas();
-                    $Visitas -> VerificaUsuario();
-                ?>
-                <h1> Café de amanhã sera de:  </h1>
-                <?php
-                    include ("sort.php");
-                    $sorts = new sorteadores();
-                    $sorts -> sortCafe();
-                ?>
-                <h1>Gerador de senha: </h1>
-                <?php
-                    $sorts -> sortPassword();
-                ?>
-            </div>      
+        <div class="container">
+            <div class="conteudo01">
+                
+            </div>
+            <div class="conteudo02">
+                <ul>
+                    <li class="slot01">
+                        <img src="./img/slot.png" alt="">
+                        <h2>Slot 01</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolore ab voluptatibus consequuntur consequatur est dolorem, dolores repellat esse sint, quasi facere. Hic quo dolores architecto quasi ex? Consequatur, fugiat?</p>
+                    </li>
+                    <li class="slot02">
+                        <img src="./img/slot.png" alt="">
+                        <h2>Slot 02</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium eveniet provident veritatis, possimus odio quibusdam nisi labore totam libero, est temporibus magnam inventore, deserunt accusantium laborum culpa dolore commodi. Repellendus!</p>
+                    </li>
+                    <li class="slot03">
+                        <img src="./img/slot.png" alt="">
+                        <h2>Slot 03</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, voluptate assumenda est enim ut officia eaque ullam cum hic, rem sapiente doloribus et obcaecati amet error accusamus. Inventore, consequuntur eos.</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="conteudo03">conteudo 3</div>
+            <div class="conteudo04">conteudo 4</div>
         </div>
     </main>
-    <footer></footer>
-    
-    
+    <footer>produzido por eu</footer>
 </body>
 </html>
